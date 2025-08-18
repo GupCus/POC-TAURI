894ebc793ab684767691169b6a0c4c31f0eeb3ee
Agus <abarrosobol@frro.utn.edu.ar>
2025-08-18 10:23:54 -0300

    Cambie la forma de las mininotes

--- src/components/MiniNote.jsx
+++ src/components/MiniNote.jsx
@@ -1,8 +1,8 @@
-export default function MiniNote({key,titulo="Lorem impsum" ,texto="dolor onet" , handler}){
-    return(
-      <article className="mininote" onClick={handler} key={key}>
-        <h3>{titulo}</h3>
-        <p>{texto}</p>
-      </article>
-    )
+export default function MiniNote({ key, titulo = "Lorem impsum", texto = "dolor onet", handler }) {
+  return (
+    <article className="mininote" onClick={handler} key={key}>
+      <h3>{titulo}</h3>
+      <p>{texto}</p>
+    </article>
+  )
 }
